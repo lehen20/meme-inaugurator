288c4100f54adf2267c27a1d71a7054e956fc934
lehen20 <zehralehen@gmail.com>
2022-01-26 01:49:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../src/components/Header.js"
+import Meme from "../src/components/Meme.js"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
