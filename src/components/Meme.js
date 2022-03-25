016415f2b6ced3939f3d10b3c5201976c349a806
lehen20 <98393493+lehen20@users.noreply.github.com>
2022-03-25 20:53:51 +0530

    Update Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -59,7 +59,7 @@ export default function Meme() {
                 </button>
             </div>
             <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
+                <img src={meme.randomImage} className="meme--image" height={350} width={700} style={{ alignSelf: 'center' }}/>
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
