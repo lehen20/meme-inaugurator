74d2c22316f70c8490dac66462ab4a2103cf3d62
lehen20 <98393493+lehen20@users.noreply.github.com>
2022-03-25 20:07:56 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ export default function Header() {
                 src="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png" 
                 className="header--image"
             />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h2 className="header--title">Meme Inaugurator</h2>
+            <h4 className="header--project">Lehen Zehra</h4>
         </header>
     )
 }
